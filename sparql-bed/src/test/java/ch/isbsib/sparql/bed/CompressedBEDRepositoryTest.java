1818ed44ddb42b6af09d929bf4b5b9ccdbadc220
Jerven Bolleman <me@jerven.eu>
2015-09-14 16:56:40 +0900

    organize imports

--- sparql-bed/src/test/java/ch/isbsib/sparql/bed/CompressedBEDRepositoryTest.java
+++ sparql-bed/src/test/java/ch/isbsib/sparql/bed/CompressedBEDRepositoryTest.java
@@ -1,27 +1,9 @@
 package ch.isbsib.sparql.bed;
 
 import java.io.File;
-import java.io.IOException;
-
-import junit.framework.TestCase;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.openrdf.model.impl.SimpleValueFactory;
-import org.openrdf.model.vocabulary.RDF;
-import org.openrdf.query.Binding;
-import org.openrdf.query.BindingSet;
-import org.openrdf.query.MalformedQueryException;
-import org.openrdf.query.QueryEvaluationException;
-import org.openrdf.query.QueryLanguage;
-import org.openrdf.query.TupleQuery;
-import org.openrdf.query.TupleQueryResult;
-import org.openrdf.repository.RepositoryException;
-import org.openrdf.repository.sail.SailRepository;
-import org.openrdf.sail.SailException;
 
 public class CompressedBEDRepositoryTest extends BEDRepositoryTest {
 
